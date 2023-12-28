35c1f017dea1b5024d88212f1e86a81280ed64ae
SHAIMAAHASSAN <shimaahassan.eng@gmail.com>
2023-12-28 08:13:42 +0200

    third commit

---
+++ lib/pages/recently_view.Page.dart
@@ -0,0 +1,107 @@
+import 'package:daily_recipe/pages/notification.page.dart';
+import 'package:daily_recipe/pages/side.menu.page.dart';
+import 'package:flutter/material.dart';
+
+class RecentlyViewedPage extends StatelessWidget {
+  const RecentlyViewedPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          leading: IconButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => SideMenuPage(),
+                    ));
+              },
+              icon: Icon(Icons.sort)),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => NotificationPage(),
+                      ));
+                },
+                icon: Icon(Icons.notifications)),
+          ]),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Recently Viewed",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    GestureDetector(
+                        onTap: () {},
+                        child: Text(
+                          "Clear",
+                          style: TextStyle(color: Colors.orange, fontSize: 18),
+                        )),
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  child: Row(
+                    children: [
+                      Expanded(
+                        flex: 2,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: "Search Using Keywords",
+                            hintStyle: TextStyle(color: Colors.grey.shade600),
+                            prefixIcon: Icon(
+                              Icons.search,
+                              color: Colors.grey.shade600,
+                              size: 20,
+                            ),
+                            filled: true,
+                            fillColor: Colors.grey.shade100,
+                            contentPadding: EdgeInsets.all(10),
+                            enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15),
+                                borderSide:
+                                    BorderSide(color: Colors.grey.shade100)),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Container(
+                        width: 50,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: Colors.grey.shade100,
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Icon(Icons.tune_sharp),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
